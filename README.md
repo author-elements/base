f322474dcb7353bde9df777e3ba6190a133f761b
Corey Butler <corey@coreybutler.com>
2019-01-20 01:47:59 -0600

    Added saucelabs tests and local test harness.

--- README.md
+++ README.md
@@ -2,9 +2,22 @@
 
 This is the base class used to build Author.io custom HTML elements (web components). It provides incredibly lightweight utilities and was designed for optimizing all `<author-*>` custom tags.
 
+[![Sauce Test Status](https://saucelabs.com/browser-matrix/author.io.svg)](https://saucelabs.com/u/author.io)
+
 ## Usage
 
 This library must be included _once_ in HTML pages _before_ any `author-*` tags.
+
+There are 4 versions of the base class available:
+
+1. *author-element.min.js* (ES6 Minified for Production)
+1. _author-element.js_ (ES6 Unminified for Debugging)
+1. *author-element.es5.min.js* (ES5 Minified for Production)
+1. _author-element.es5.js_ (ES5 Unminified for Debugging)
+
+Each version has it's own source map, so it's always possible to trace activity back to a specific code block in the source.
+
+You only need to choose one of these files. If you need to support Internet Explorer, older versions of Chrome/Firefox/Safari, then you likely need the ES5 version.
 
 *Via Global CDN*
 
@@ -18,6 +31,12 @@ This library must be included _once_ in HTML pages _before_ any `author-*` tags.
 ```
 
 *Via npm*
+
+First install the module locally:
+
+`npm install @author.io/element-base -S`
+
+Then include it in your HTML:
 
 ```html
 <html>
--- karma.conf.es5.js
+++ karma.conf.es5.js
@@ -1,96 +1,11 @@
-const base = require('./karma.base')
+const base = require('./karma.base')('test/es5')
+const files = base.getFiles('./dist/author-element.es5.js')
 
-var getFiles = function () {
-  var files = [
-    {
-      pattern: require('path').join(process.cwd(), './dist/author-element.es5.js'),
-      nocache: true
-    }
-  ]
-
-  // Run all tests by default
-  let testfiles = 'test/es5/*.js'
-
-  return files.concat([
-    testfiles,
-    'test/es5/test.html'
-  ])
-}
-
-console.log(base.tablemaker([[base.chalk.bold('Included Files')]].concat(getFiles().map(file => { return [file] }))))
+base.displayFiles(files)
 
 module.exports = function (config) {
-  config.set({
-
-    plugins: [
-      require('karma-browserify'),
-      require('tape'),
-      require('karma-tap'),
-      require('karma-spec-reporter'),
-      require('karma-chrome-launcher'),
-      // require('karma-firefox-launcher'),
-      // require('karma-safari-launcher'),
-      // require('karma-ie-launcher'),
-      // require('karma-edge-launcher'),
-      // require('karma-phantomjs-launcher'),
-      // require('karma-sauce-launcher'),
-      require('karma-html2js-preprocessor')
-    ],
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['tap', 'browserify'],
-
-    // list of files / patterns to load in the browser
-    files: getFiles(),
-
-    // list of files to exclude
-    exclude: [],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'test/es5/**/*.js': ['browserify'],
-      'test/es5/test.html': 'html2js'
-      // , 'test/lib/**/*.js': 'coverage'
-    },
-
-    // coverageReporter: {
-    //   type : 'html',
-    //   dir : 'coverage/'
-    // },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: base.reporterEngines, // ['progress'],
-
-    // web server port
-    port: 9876,
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_DEBUG,
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultanous
-    concurrency: 3
-  })
+  config.set(Object.assign(base.configuration, {
+    files,
+    logLevel: config.LOG_INFO
+  }))
 }
