33842c89f0afaa9f776fd38064c7ec2b1afd6950
Corey Butler <corey@coreybutler.com>
2019-02-01 18:14:01 -0600

    Try removing platforms to prevent saucelabs disconnects.

--- karma.conf.es5.js
+++ karma.conf.es5.js
@@ -1,3 +1,15 @@
 const customize = require('@author.io/karma-customelements')('test/es5', './dist/author-element.es5.js')
 
-module.exports = config => customize(config)
+module.exports = config => {
+  config.set(Object.assign(customize(config), {
+    concurrency: 1
+  }))
+
+  let launchers = {}
+  Object.keys(config.customLaunchers).forEach(l => {
+    launchers[l] = config.customLaunchers[l]
+    delete launchers[l].platform
+  })
+
+  config.customLaunchers = launchers
+}
