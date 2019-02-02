0f50c316a66f7517a07c5515bc7eb760399df8a2
Corey Butler <corey@coreybutler.com>
2019-02-01 20:35:05 -0600

    Updated ES6 version to only run on modern browsers.

--- karma.conf.es6.js
+++ karma.conf.es6.js
@@ -1,7 +1,7 @@
 const customize = require('@author.io/karma-customelements')('test/es6', './dist/author-element.js')
 
 module.exports = config => {
-  config.set(Object.assign(customize(config), {
+  config.set(Object.assign(customize(config, true, false), {
     browserify: {
       transform: [ 'rollupify' ]
     }
