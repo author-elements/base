a866614a8a81a4fe1e1886d9b192b61fa0946b33
Corey Butler <corey@coreybutler.com>
2019-02-01 18:19:28 -0600

    Adding platforms back to tests.

--- karma.conf.es5.js
+++ karma.conf.es5.js
@@ -1,15 +1,3 @@
 const customize = require('@author.io/karma-customelements')('test/es5', './dist/author-element.es5.js')
 
-module.exports = config => {
-  config.set(Object.assign(customize(config), {
-    concurrency: 1
-  }))
-
-  let launchers = {}
-  Object.keys(config.customLaunchers).forEach(l => {
-    launchers[l] = config.customLaunchers[l]
-    delete launchers[l].platform
-  })
-
-  config.customLaunchers = launchers
-}
+module.exports = config => customize(config)
