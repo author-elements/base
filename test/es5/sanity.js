8e5aac2ad2f9396f9f6bcc27ccc3e694e53b5b7d
Corey Butler <corey@coreybutler.com>
2019-02-02 22:14:59 -0600

    Updated test config (trying to get sauce labs to work)

--- test/es5/sanity.js
+++ test/es5/sanity.js
@@ -1,8 +1,6 @@
 var test = require('tape')
 
-setTimeout(function () {
-  test('Sanity', function (t) {
-    t.ok(typeof AuthorElement === 'function', 'Base class detected.')
-    t.end()
-  })
-}, 8000)
+test('Sanity', function (t) {
+  t.ok(typeof AuthorElement === 'function', 'Base class detected.')
+  t.end()
+})
