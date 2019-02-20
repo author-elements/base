ba0d42239f16beb91e97607b04d551ffdea9a2a6
Corey Butler <corey@coreybutler.com>
2019-02-19 22:01:37 -0600

    Updated w/ BrowserStack settings.

--- karma.conf.es5.js
+++ karma.conf.es5.js
@@ -1,4 +1,4 @@
-let customize = require('@author.io/karma-customelements')('test/es5', './dist/author-element.es5.js', 'SauceLabs')
+let customize = require('@author.io/karma-customelements')('test/es5', './dist/author-element.es5.js', 'BrowserStack')
 //
 // module.exports = config => {
 //   config.set(Object.assign(customize(config), {
