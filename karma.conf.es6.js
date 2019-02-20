aaeca1d877d10d7440e1061b30a0fb644482d5b0
Corey Butler <corey@coreybutler.com>
2019-02-20 00:30:43 -0600

    Updated test for debugging purposes

--- karma.conf.es6.js
+++ karma.conf.es6.js
@@ -1,4 +1,4 @@
-const customize = require('@author.io/karma-customelements')('test/es6', './dist/author-element.js')
+const customize = require('@author.io/karma-customelements')('test/es6', './dist/author-element.js', 'BrowserStack')
 
 module.exports = config => {
   config.set(Object.assign(customize(config, true, false), {
