38e5b094d9179590f78d855d499567c1c500c7ae
Corey Butler <corey@coreybutler.com>
2019-02-24 22:19:47 -0600

    Updated filename in karma test suites

--- karma.conf.es5.js
+++ karma.conf.es5.js
@@ -1,11 +1,4 @@
-let customize = require('@author.io/karma-customelements')('test/es5', './dist/author-element.es5.js', 'BrowserStack')
-//
-// module.exports = config => {
-//   config.set(Object.assign(customize(config), {
-//     concurrency: 1
-//   }))
-// }
-
+let customize = require('@author.io/karma-customelements')('test/es5', './dist/author-base.es5.js', 'BrowserStack')
 // require('leaked-handles').set({
 //   // fullStack: true, // use full stack traces
 //   timeout: 10000 // run every 30 seconds instead of 5.
@@ -22,40 +15,3 @@ module.exports = config => {
     logLevel: config.LOG_INFO
   }))
 }
-// let preprocessors = (root) => {
-//   let cfg = {}
-//
-//   cfg[`${root}/**/*.js`] = ['browserify']
-//   cfg[`${root}/test.html`] = 'html2js'
-//
-//   return cfg
-// }
-//
-// let custombase = Object.assign({}, base)
-// delete custombase.getFiles
-// custombase.getFiles = (root, file, module = false) => {
-//   let mainfile = {
-//     pattern: path.join(process.cwd(), file),
-//     nocache: true
-//   }
-//
-//   if (module) {
-//     mainfile.type = 'module'
-//   }
-//
-//   return [
-//     mainfile,
-//     path.join(root, '/*.js'),
-//     path.join(root, 'test.html')
-//   ]
-// }
-//
-// module.exports = function(config) {
-//   config.set(Object.assign(custombase.configuration, {
-//     browserStack: {
-//       username: 'coreybutler2',
-//       accessKey: '5Myqq6xsMexxpq4YeHRT'
-//     },
-//     concurrency: 1
-//   }))
-// }
--- karma.conf.es6-modules.js
+++ karma.conf.es6-modules.js
@@ -1,4 +1,4 @@
-const customize = require('@author.io/karma-customelements')('test/es6-modules', './dist/author-element.js', 'BrowserStack')
+const customize = require('@author.io/karma-customelements')('test/es6-modules', './dist/author-base.js', 'BrowserStack')
 
 module.exports = config => {
   config.set(Object.assign(customize(config, true, true), {
